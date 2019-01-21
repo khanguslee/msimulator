7637200025a7a44865c0a13e5ea2c607c5b11c86
khanguslee <khanguslee@gmail.com>
2019-01-22 10:22:01 +1100

    Run program with multiple processors

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import time 
 import argparse
 import random
@@ -14,6 +15,20 @@ parser.add_argument('--help', action='help', help='show this help message and ex
 parser.add_argument("time", help="how long to run simulator in seconds", type=int)
 parser.add_argument("-h", "--host", help="IP address of the mongoDB you want to connect to", default="localhost")
 parser.add_argument("-p", "--port", help="port of the mongoDB you want to connect to", default=27017, type=int)
+parser.add_argument("-m", "--minutes", help="change time to minutes", action="store_true")
+parser.add_argument("-n", "--number", help="number of processes to spawn", default=1, type=int)
+
+
+def sum_results(input_results_array):
+    output_results = {}
+    for result in input_results_array:
+        for key,value in result.items():
+            if key not in output_results:
+                output_results[key] = value
+            else:
+                output_results[key] += value
+
+    return output_results
 
 def generate_data(input_names):
     # Mandatory data
@@ -46,28 +61,21 @@ def initialise_data(number_of_names):
         output_name_array.append(generated_name)
     return output_name_array
 
-def connect_to_test_collection(host, port):
+def connect_to_test_collection(host, port, process_id):
     # TODO: Check if user already has this database and collection
     client = pymongo.MongoClient(host, port)
     db = client["msimulator"]
-    collection = db["test"]
+    collection = db["test-" + str(process_id)]
     return collection
 
-if __name__ == "__main__":
-    # Get argument values
-    args = parser.parse_args()
-    input_run_time = args.time
-    mongodb_ip_address = args.host
-    mongodb_port = args.port
-
+def run(process_id, input_run_time, mongodb_ip_address, mongodb_port, queue):
     # Setup
-    collection = connect_to_test_collection(mongodb_ip_address, mongodb_port)
+    collection = connect_to_test_collection(mongodb_ip_address, mongodb_port, process_id)
     first_name_array = initialise_data(100)
-    
-    number_of_operations = {}
-
     # Get all scenario functions
     test_scenarios = dir(scenarios)[9:]
+
+    number_of_operations = {}
     initial_time = time.time()
     while True:
         chosen_scenario = chance.pickone(test_scenarios)
@@ -86,6 +94,37 @@ if __name__ == "__main__":
         total_time = current_time - initial_time
         if (total_time > input_run_time):
             break
-
     cleanup_database(collection)
-    print(number_of_operations)
+
+    # Store results
+    queue.put(number_of_operations)
+
+if __name__ == "__main__":
+    # Get argument values
+    args = parser.parse_args()
+    input_run_time = args.time
+    is_minutes = args.minutes
+
+    if is_minutes:
+        print("Running for " + str(input_run_time) + " minutes")
+        input_run_time *= 60 
+    else:
+        print("Running for " + str(input_run_time) + " seconds")
+
+    mongodb_ip_address = args.host
+    mongodb_port = args.port
+
+    processes = []
+    queue = multiprocessing.Queue()
+    number_of_processes = args.number
+    for process_id in range(number_of_processes):
+        process = multiprocessing.Process(target=run, args=(process_id,input_run_time, mongodb_ip_address,mongodb_port,queue,))
+        processes.append(process)
+        process.start()
+
+    for proess in processes:
+        process.join()
+
+    results = [queue.get() for process in processes]
+    results_sum = sum_results(results)
+    print(results_sum)
